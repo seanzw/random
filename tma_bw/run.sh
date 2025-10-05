03d24a5c31bd865978c4bb79fa46ad2e5529e887
Zhengrong Wang <seanyukigeek@gmail.com>
2025-10-05 10:01:31 +0800

    lock the frequence of GPU to 2GHz

--- tma_bw/run.sh
+++ tma_bw/run.sh
@@ -8,9 +8,16 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
+echo "Setting GPU to 2GHz for consistent benchmarking..."
+sudo nvidia-smi --lock-gpu-clocks=2000,2000
+
 echo ""
 echo "Running bandwidth comparison test..."
 ./bandwidth_test.out > bw_result.log
+
+echo "Resetting GPU clocks to default..."
+sudo nvidia-smi --reset-gpu-clocks
+
 python process.py -i bw_result.log -o bw_result.png --peak-bw 5.6
 
 echo ""
